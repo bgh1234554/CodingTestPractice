6ea1d083339ebdf3bd0df015b683eeb221d8efc6
baekJiHoon <bgh1234554@gmail.com>
2025-01-09 18:42:36 +0900

    Feat: 11720, 11659 풀이 추가

--- src/DoIt/Chapter03_1/BaekJoon11720.java
+++ src/DoIt/Chapter03_1/BaekJoon11720.java
@@ -14,5 +14,7 @@ public class BaekJoon11720 { //채점시에는 Main으로 수정해야 한다.
         }
         sb.append(sum);
         System.out.println(sb);
+        //str을 받은 다음에 .toCharArray 메서드를 통해 char[]로 변환 후,
+        //똑같이 for문 돌면서 더해도 된다.
     }
 }
