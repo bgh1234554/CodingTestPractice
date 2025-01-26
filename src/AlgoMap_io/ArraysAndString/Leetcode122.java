fc5c948058b6fa1c6cd26f9d7450b04d41f106c3
baekJiHoon <bgh1234554@gmail.com>
2025-01-27 02:33:50 +0900

    println함수 추가

--- src/AlgoMap_io/ArraysAndString/Leetcode122.java
+++ src/AlgoMap_io/ArraysAndString/Leetcode122.java
@@ -11,7 +11,7 @@ Find and return the maximum profit you can achieve.
  */
 public class Leetcode122 {
     public static void main(String[] args) {
-        maxProfit(new int[]{7,1,5,3,6,4});
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
     }
     public static int maxProfit(int[] prices) {
         int total=0;
