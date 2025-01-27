b3832fb5d9673b3b38ebc096230d8c3bd350e34a
baekJiHoon <bgh1234554@gmail.com>
2025-01-27 23:10:29 +0900

    Leetcode 56, 75 풀이 추가

---
+++ src/AlgoMap_io/ArraysAndString/Leetcode56.java
@@ -0,0 +1,61 @@
+package AlgoMap_io.ArraysAndString;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Leetcode56 {
+    public static void main(String[] args) {
+        System.out.println(Arrays.deepToString(merge(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}})));
+    }
+    public static int[][] merge(int[][] intervals){
+        if(intervals.length==0) return new int[0][0];
+        //왜인지 모르겠지만, 정렬이 안되어있는 배열도 주어지니까, 정렬을 해버리자.
+        Arrays.sort(intervals,(a,b)->a[0]-b[0]);
+        //List안에 클래스만 담을 수 있는 줄 알았는데 배열도 가능하다...
+        List<int[]> merged = new ArrayList<int[]>();
+        for(int[] interval : intervals){
+            //겹치는 구간이 발생하려면 다음 구간의 시작 구간이 이전 구간의 최댓값 안에 있어야 하니까.
+            //그렇지 않을 경우에 새 구간을 만든다.
+            if(merged.isEmpty()||merged.get(merged.size()-1)[1]<interval[0]){
+                merged.add(interval);
+            }
+            else{
+                merged.get(merged.size()-1)[1]=Math.max(merged.get(merged.size()-1)[1],interval[1]);
+            }
+        }
+        //List<int[]>를 int[][]로 바꾸는 방법.
+        return merged.toArray(new int[merged.size()][]);
+    }
+//    public static void main(String[] args) {
+//        System.out.println(Arrays.deepToString(merge(new int[][]{{1, 3}, {2, 6}, {8, 10}, {15, 18}})));
+//    }
+//    public static int[][] merge(int[][] intervals) {
+//        boolean[] visited = new boolean[10001];
+//        int max=0;
+//        for(int i=0;i<intervals.length;i++){
+//            for(int j=intervals[i][0];j<=intervals[i][1];j++){
+//                if(!visited[j]){
+//                    visited[j]=true;
+//                    max=j;
+//                }
+//            }
+//        }
+//        int[][] ans = new int[intervals.length][2];
+//        int count=0;
+//        for(int i=0;i<=max;i++){
+//            while(!visited[i]){
+//                i++;
+//            }
+//            int start=i;
+//            while(visited[i]){
+//                i++;
+//            }
+//            int end=i-1;
+//            ans[count]=new int[]{start,end};
+//            count++;
+//        }
+//        return Arrays.copyOfRange(ans,0,count);
+//    }
+    // 문제를 잘못 이해했다.
+}
--- src/AlgoMap_io/ArraysAndString/Leetcode75.java
+++ src/AlgoMap_io/ArraysAndString/Leetcode75.java
@@ -1,5 +1,23 @@
 package AlgoMap_io.ArraysAndString;
 
 public class Leetcode75 {
-    
+    public void sortColors(int[] nums) {
+        int[] count = new int[3];
+        for(int num : nums){
+            count[num]++;
+        }
+        int i=0;
+        for(int j=0;j<3;j++){
+            while(count[j]>0){
+                nums[i]=j;
+                i++;
+                count[j]--;
+            }
+        }
+    }
 }
+/*
+전형적인 계수정렬을 이용하는 문제. 값의 범위가 매우 좁고, 배열의 크기도 짧기 때문에
+그냥 계수정렬을 써서 출력하면 매우 간단하다.
+왠지 모르겠지만 자동저장에 실패해서 1월 25일에 Leetcode에 풀었는데 지금 풀이를 적는다.
+ */
