0ce1f9582a8220033130a4265f08815904c87c97
baekJiHoon <bgh1234554@gmail.com>
2025-01-28 23:43:02 +0900

    Spiral Matrix 문제 및 소감 추가

--- src/AlgoMap_io/ArraysAndString/Leetcode54.java
+++ src/AlgoMap_io/ArraysAndString/Leetcode54.java
@@ -34,4 +34,41 @@ public class Leetcode54 {
         }
         return ans.subList(0,m*n);
     }
+    public static List<Integer> spiralOrderCopilot(int[][] matrix) {
+        List<Integer> ans = new ArrayList<>();
+        if (matrix == null || matrix.length == 0) return ans;
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                ans.add(matrix[top][i]);
+            }
+            top++;
+            for (int i = top; i <= bottom; i++) {
+                ans.add(matrix[i][right]);
+            }
+            right--;
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    ans.add(matrix[bottom][i]);
+                }
+                bottom--;
+            }
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    ans.add(matrix[i][left]);
+                }
+                left++;
+            }
+        }
+        return ans;
+    }
 }
+/*
+처음에 보면 이게 뭔지 싶으나, 차분히 생각해보면 된다. 어떤 방식으로 위, 오른쪽, 아래, 왼쪽을 탐색할 건지 생각하고
+각 탐색에 대한 일반식을 만들어보려고 노력해보면 된다.
+이전의 rotate matrix도 그렇고 오히려 행렬 관련을 더 잘맞히는 것 같다. 힌트를 하나도 안보고 혼자 고민해서 풀어서
+기분이 좋다.
+변수를 더 많이 선언하지만, 보기엔 더 간결하고 이쁜 코파일럿이 만든 방식도 있으니 참고하면 좋다.
+상한선, 하한선, 우한선, 좌한선을 각각 설정해 한번 탐색할때마다 경계를 변경해주는 방식이다.
+ */
