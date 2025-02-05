a60756c80ca756e6321d1df8b3c5cd7e887bc4b6
baekJiHoon <bgh1234554@gmail.com>
2025-02-05 15:36:46 +0900

    Chapter 08-3 위상 정렬 어려운 문제 제외 풀이 완료

---
+++ src/DoIt/Chapter08/Chapter08_03/BaekJoon1516.java
@@ -0,0 +1,60 @@
+package DoIt.Chapter08.Chapter08_03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BaekJoon1516 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Integer>[] graph = new ArrayList[N+1];
+        for(int i=1;i<=N;i++){
+            graph[i]=new ArrayList<>();
+        }
+        int[] indegree = new int[N+1];
+        int[] buildCost = new int[N+1];
+        int[] ans = new int[N+1];
+        for(int i=1;i<=N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            buildCost[i]=Integer.parseInt(st.nextToken()); //얘네가 화살표를 받는 쪽이다.
+            //그래프를 그려보면 왜 그런지 알 수 있다.
+            while(true){
+                int connection = Integer.parseInt(st.nextToken());
+                if(connection==-1) break;
+                graph[connection].add(i); //connection번 건물을 지어야 i번 건물을 지을 수 있으니까.
+                indegree[i]++;
+            }
+        }
+        //위상 정렬 수행
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1;i<=N;i++){
+            if(indegree[i]==0){
+                queue.add(i);
+            }
+        }
+        while(!queue.isEmpty()){
+            int parent = queue.poll();
+            for(int child:graph[parent]){
+                indegree[child]--;
+                //왜 Math.max가 들어가는가?
+                //건물을 짓는 방법이 여러가지일 수 있기 때문에??
+                //1->4->3, 1->2->3 이런 상황이라면...?
+                //1->2->3, 1->4->3의 관계인 경우
+                //1을 짓는데는 10이 들고, 2를 짓는데는 5, 4를 짓는데는 3이 들 때,
+                //1을 짓고 2를 짓는동안 4를 다 지을 수 있으니까, 4를 짓는 시간을 포함시키지 않기 위해
+                ans[child]=Math.max(ans[child],ans[parent]+buildCost[parent]);
+                if(indegree[child]==0){
+                    queue.add(child);
+                }
+            }
+        }
+        for(int i=1;i<=N;i++){
+            System.out.println(ans[i]+buildCost[i]);
+        }
+    }
+}
---
+++ src/DoIt/Chapter08/Chapter08_03/BaekJoon2252.java
@@ -0,0 +1,47 @@
+package DoIt.Chapter08.Chapter08_03;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class BaekJoon2252 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); //Node
+        int M = sc.nextInt(); //Edge
+        ArrayList<Integer>[] graph = new ArrayList[N+1];
+        for(int i=1;i<=N;i++){
+            graph[i]=new ArrayList<>();
+        }
+        int[] indegree = new int[N+1];
+        for(int i=0;i<M;i++){
+            int S = sc.nextInt();
+            int E = sc.nextInt();
+            graph[S].add(E);
+            indegree[E]++;
+        }
+        //위상 정렬 시작
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=1;i<=N;i++){
+            //진입 차수가 0이 되는 노드 순차적으로 추가
+            if(indegree[i]==0){
+                queue.add(i);
+            }
+        }
+        //BFS와 비슷한 방식인 것 같다.
+        while(!queue.isEmpty()){
+            //큐에 있는 노드를 꺼내면서
+            int now = queue.poll();
+            System.out.print(now+" ");
+            for(int node:graph[now]){
+                //노드와 연결된 에지를 전부 하나씩 제거
+                indegree[node]--;
+                if(indegree[node]==0){
+                    //노드의 진입차수가 0이 되면 큐에 추가한다.
+                    queue.add(node);
+                }
+            }
+        }
+    }
+}
