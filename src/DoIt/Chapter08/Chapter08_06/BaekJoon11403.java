a362dc6392cf6cbdf7be441e93e233353461d99b
baekJiHoon <bgh1234554@gmail.com>
2025-02-08 23:22:43 +0900

    Flyod-Warshall Algorithm 문제 풀이 완료

---
+++ src/DoIt/Chapter08/Chapter08_06/BaekJoon11403.java
@@ -0,0 +1,41 @@
+package DoIt.Chapter08.Chapter08_06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon11403 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[][] matrix = new int[N][N];
+        for(int i=0;i<N;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for(int j=0;j<N;j++){
+                matrix[i][j]=Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int k=0;k<N;k++){
+            for(int s=0;s<N;s++){
+                for(int e=0;e<N;e++){
+                    if(matrix[s][k]==1&&matrix[k][e]==1){
+                        matrix[s][e]=1;
+                    }
+                }
+            }
+        }
+        for(int i=0;i<N;i++){
+            for(int j=0;j<N;j++){
+                System.out.print(matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
+/*
+이전 문제보다 더 쉽다.
+이번에는 길의 거리가 아니라, 노드와 노드 사이의 에지의 존재 여부만 보여준다.
+distance[S][E] = Math.min(distance[S][E],distance[S][K]+distance[K][E]);
+를 적용하려면, distance[S][K]와 distance[K][E]가 존재하기만 하면 존재한다고 업데이트해주면 된다.
+ */
---
+++ src/DoIt/Chapter08/Chapter08_06/BaekJoon1389.java
@@ -0,0 +1,52 @@
+package DoIt.Chapter08.Chapter08_06;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon1389 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] relation = new int[N+1][N+1];
+        for(int i=1;i<=N;i++){
+            for(int j=1;j<=N;j++){
+                if(i==j) relation[i][j]=0;
+                else relation[i][j]=100000;
+            }
+        }
+        for(int i=0;i<M;i++){
+            st = new StringTokenizer(br.readLine());
+            int S = Integer.parseInt(st.nextToken());
+            int E = Integer.parseInt(st.nextToken());
+            relation[S][E]=1; relation[E][S]=1;
+        }
+        for(int k=1;k<=N;k++){
+            for(int s=1;s<=N;s++){
+                for(int e=1;e<=N;e++){
+                    relation[s][e]=Math.min(relation[s][e],relation[s][k]+relation[k][e]);
+                }
+            }
+        }
+        int min = Integer.MAX_VALUE; int index=0;
+        for(int i=1;i<=N;i++){
+            int num=0;
+            for(int j=1;j<=N;j++){
+                num+=relation[i][j];
+            }
+            if(min>num){
+                index=i;
+                min=num;
+            }
+        }
+        System.out.println(index);
+    }
+}
+/*
+친구 관계 -> 각 도시 간의 도로 관계라고 생각하면 된다. 11403번의 응용버전.
+BFS로도 풀 수 있다.
+인접 배열 초기화 할때 Integer.MAX_VALUE 로 초기화하지 않기.
+ */
