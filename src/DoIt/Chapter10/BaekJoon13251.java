05346ebde5b5bf4f6430f0f624782099f3148c80
baekJiHoon <bgh1234554@gmail.com>
2025-02-15 18:10:35 +0900

    Chapter 10 13251번 파일 이름 변경

--- src/DoIt/Chapter10/BaekJoon13251.java
+++ src/DoIt/Chapter10/BaekJoon13251.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BaekJoon13251_NoCombination {
+public class BaekJoon13251 {
     public static void main(String[] args) throws IOException {
         int[] stones = new int[51];
         double[] probability = new double[51];
--- src/DoIt/Chapter10/BaekJoon13251OverFlow.java
+++ src/DoIt/Chapter10/BaekJoon13251OverFlow.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class BaekJoon13251 {
+public class BaekJoon13251OverFlow {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int M = Integer.parseInt(br.readLine());
