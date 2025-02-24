67f45f0b0edfd21530c105b7266c857e1f9474f2
baekJiHoon <bgh1234554@gmail.com>
2025-02-24 17:21:02 +0900

    Do It Chapter 12 기하 풀이 시작

---
+++ src/DoIt/Chapter12_Geometry/BaekJoon11758.java
@@ -0,0 +1,22 @@
+package DoIt.Chapter12_Geometry;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BaekJoon11758 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] x = new int[3]; int[] y = new int[3];
+        for(int i=0;i<3;i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x[i]=Integer.parseInt(st.nextToken());
+            y[i]=Integer.parseInt(st.nextToken());
+        }
+        int ccw = x[0]*y[1]+x[1]*y[2]+x[2]*y[0]-x[3]*y[2]-x[2]*y[1]-x[1]*y[3];
+        if(ccw<0) System.out.println(-1);
+        else if(ccw>0) System.out.println(1);
+        else System.out.println(0);
+    }
+}
