631d69b5ac3dd629bfb365453522141247894ab7
baekJiHoon <bgh1234554@gmail.com>
2025-03-06 10:44:33 +0900

    2807을 커밋해아하는데 206을 커밋해버림

--- src/AlgoMap_io/LinkedList/Leetcode2807.java
+++ src/AlgoMap_io/LinkedList/Leetcode2807.java
@@ -1,4 +1,23 @@
 package AlgoMap_io.LinkedList;
 
 public class Leetcode2807 {
+    public static void main(String[] args) {
+
+    }
+    public ListNode insertGreatestCommonDivisors(ListNode head){
+        ListNode cur = head;
+        while(cur.next!=null){
+            int gcd = gcd(cur.val,cur.next.val);
+            ListNode newNode = new ListNode(gcd);
+            newNode.next = cur.next;
+            cur.next = newNode;
+            cur = cur.next.next;
+        }
+        return head;
+    }
+    //최대 공약수 구하는 법, 계속 까먹으니까 다시 알아두자.
+    public static int gcd(int a, int b){
+        if(b==0) return a;
+        return gcd(b,a%b);
+    }
 }
