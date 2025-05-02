28046fa9921f4324ed18a8305b3cd535d600d0a0
baekJiHoon <bgh1234554@gmail.com>
2025-05-02 14:50:28 +0900

    Leetcode 79 문제 설명 주석에 추가

--- src/AlgoMap_io/RecursiveBacktracking/Leetcode79.java
+++ src/AlgoMap_io/RecursiveBacktracking/Leetcode79.java
@@ -2,7 +2,14 @@ package AlgoMap_io.RecursiveBacktracking;
 
 import java.util.ArrayList;
 import java.util.List;
+/*
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
 
+The word can be constructed from letters of sequentially adjacent cells,
+where adjacent cells are horizontally or vertically neighboring.
+
+The same letter cell may not be used more than once.
+ */
 public class Leetcode79 {
     public static void main(String[] args) {
         System.out.println(exist(new char[][]{{'A','B','C','E'},{'S','F','C','S'},{'A','D','E','E'}},"ABCCED"));
