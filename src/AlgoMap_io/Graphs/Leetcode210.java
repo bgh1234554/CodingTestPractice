381d8d191595e1052b253fe1e8f1baac5fe26bed
baekJiHoon <bgh1234554@gmail.com>
2025-05-08 14:25:51 +0900

    20250508 feat: algomap.io graphs 2일차 종료

---
+++ src/AlgoMap_io/Graphs/Leetcode210.java
@@ -0,0 +1,76 @@
+package AlgoMap_io.Graphs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+/*
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1.
+You are given an array prerequisites where prerequisites[i] = [ai, bi]
+indicates that you must take course bi first if you want to take course ai.
+
+For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+
+Return the ordering of courses you should take to finish all courses.
+If there are many valid answers, return any of them.
+If it is impossible to finish all courses, return an empty array.
+*/
+public class Leetcode210 {
+    public static void main(String[] args) {
+        //int[]의 내용을 출력하는 방법 - Arrays.toString()
+        System.out.println(Arrays.toString(findOrder(4, new int[][]{{1, 0}, {2, 0}, {3, 1}, {3, 2}})));
+        System.out.println(Arrays.toString(findOrder(2, new int[][]{{1, 0}, {0,1}})));
+    }
+    public static int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<Integer>[] graph = new ArrayList[numCourses];
+        int[] indegree = new int[numCourses];
+        for(int i=0;i<numCourses;i++){
+            graph[i] = new ArrayList<>();
+        }
+        for(int[] edge : prerequisites){
+            //b를 수강해야 a를 들을 수 있으니까...
+            graph[edge[1]].add(edge[0]);
+            indegree[edge[0]]++; //진입차수 추가
+        }
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]==0){
+                queue.offer(i);
+            }
+        }
+        int[] ans = new int[numCourses];
+        int index = 0;
+        while(!queue.isEmpty()){
+            int prerequisite = queue.poll();
+            ans[index++] = prerequisite;
+            for(int i : graph[prerequisite]){
+                indegree[i]--;
+                if(indegree[i]==0){
+                    queue.offer(i);
+                }
+            }
+        }
+//        if(index!=numCourses){
+//            return new int[]{};
+//        }
+//        return ans;
+        //삼항연산자 사용해서 이쁘게 적는 법
+        return index==numCourses ? ans : new int[0];
+    }
+}
+/*
+정말 오랜만에 풀어본 위상정렬 문제라 어려웠다.
+
+선수 조건에 모순이 있어서 false를 리턴한다는 말은, 선수 과목 조건의 관계도를 그래프로 나타냈을 때
+사이클이 존재하는지의 유무를 탐색하는 것과 동일하다.
+
+이는 위상정렬 알고리즘으로 풀 수 있는데, 너무 오랜만에 풀어서 당황했다.
+
+위상 정렬 준비물
+- ArrayList의 배열로 표현된 그래프
+- 연결 차수를 나타내는 indegree 배열
+- BFS를 위한 큐 (DFS로 풀 수 있다곤 하는데 잘 모르겠다)
+- 각 노드를 탐색 할 때마다 연결된 노드의 indegree 차수를 1씩 감소시킴
+- 연결 차수가 0이 되면 큐에 추가 후 탐색...
+ */
